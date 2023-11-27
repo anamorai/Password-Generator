9687566cadc2ae62371c8307f8379754c6a35d98
Anastasia Moraites <anastasiamoraites@gmail.com>
2023-11-27 20:52:31 +0200

    added code to my js file to enable the password generator to output a value

--- script.js
+++ script.js
@@ -100,41 +100,59 @@ function getPasswordOptions() {
     alert('Please enter a NUMBER between 8 - 128')
     return getPasswordOptions();
   }
-
+  let newarray = []
   var lowercase = confirm("Would you like lowercase characters in your password? Click 'Ok' for yes or 'Cancel' for no.")
+  if (lowercase === true) {
+    newarray = newarray.concat(lowerCasedCharacters)
+  }
   var uppercase = confirm("Would you like uppercase characters in your password? Click 'Ok' for yes or 'Cancel' for no.")
+  if (uppercase === true) {
+    newarray = newarray.concat(upperCasedCharacters)
+  }
   var numeric = confirm("Would you like numberic characters in your password? Click 'Ok' for yes or 'Cancel' for no.")
+  if (numeric === true) {
+    newarray = newarray.concat(numericCharacters)
+  }
   var special = confirm("Would you like special characters in your password? Click 'Ok' for yes or 'Cancel' for no.")
-  if (!lowercase && !uppercase && !numeric && !special) {alert('Please choose at least one character type')
-  return getPasswordOptions();
+  if (special === true) {
+    newarray = newarray.concat(specialCharacters)
+  }
+  console.log(newarray);
+
+  if (!lowercase && !uppercase && !numeric && !special) {
+    alert('Please choose at least one character type')
+    return getPasswordOptions();
+  }
+
+  // Checking if the special character selection was working:
+  // var userOptions = {
+  //   lowercase: lowercase,
+  //   uppercase: uppercase,
+  //   numeric: numeric,
+  //   special: special,
+  // };
+  // console.log(userOptions)
+
+  // return userOptions;
+
+
+  // Generating a new password from new array
+  let randompassword = ''
+  for (let i = 0; i < passwordLength; i++) {
+    console.log("Inside IF loop");
+    var randomindex = Math.floor(Math.random() * newarray.length)
+    randompassword = randompassword + newarray[randomindex]
+  }
+  return randompassword
 }
 
-var userOptions = {
-  lowercase:lowercase,
-  uppercase:uppercase,
-  numeric:numeric,
-  special:special,
-};
-console.log(userOptions)
-return userOptions;
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
-function generatePassword() {
-var options = getPasswordOptions();
-}
-
-// Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = getPasswordOptions();
+  console.log("Password");
+  console.log(password);
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
@@ -144,4 +162,26 @@ function writePassword() {
 generateBtn.addEventListener('click', writePassword);
 
 
+////////////////////////
+
+// // Function for getting a random element from an array
+// function getRandom(arr) {
+
+// }
+
+// // Function to generate password with user input
+// function generatePassword() {
+//   var options = getPasswordOptions();
+// }
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector('#generate');
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
+
+//   passwordText.value = password;
+// }
 
